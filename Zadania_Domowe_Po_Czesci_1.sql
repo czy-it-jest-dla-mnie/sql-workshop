f398ea958d6b10a86f4175cf913b3a02eb51a450
filipp <filip.paluch@o2.pl>
2022-02-21 21:55:48 +0100

    Zadania domowe po czesc 1

---
+++ Zadania_Domowe_Po_Czesci_1.sql
@@ -0,0 +1,6 @@
+1. Wypisz nazwiska pierwszych 2 użytkowników którzy są najmłodsi
+2. Wypisz wszystkie dane użytkowników którzy są poniżej 18 roku lub powyżej 30 i mieszkają w Polsce
+3. Wypisz Imiona użytkowników w kolejności alfabetycznej
+4. * Znajdz użytkownikow których wiek jest liczbą parzystą
+5. * Wypisz połączone imię i nazwisko wszystkich użytkowników w formie jednej kolumny o nazwie 'UserName'.
+   Format wypisanych danych to 'Filip Paluch' jako jedna kolumna zamiast dwóch kolumn. 
